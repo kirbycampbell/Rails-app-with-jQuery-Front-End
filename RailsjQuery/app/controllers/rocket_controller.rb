33f0d26d2c990b9765d5cfc175bc3f08dcff0ebb
Kirby <jkirbycampbell@gmail.com>
2018-11-01 15:31:14 -0700

    Controllers Set up

---
+++ RailsjQuery/app/controllers/rocket_controller.rb
@@ -0,0 +1,2 @@
+class RocketController < ApplicationController
+end
---
+++ RailsjQuery/test/controllers/rocket_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RocketControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
